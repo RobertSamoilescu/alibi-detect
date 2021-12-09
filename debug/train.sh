60350166e7bd4779c9c887c51a9e8f67f48ca14c
Robert Samoilescu <robert.samoilescu@gmail.com>
2021-12-09 16:22:23 +0000

    small changes

--- debug/train.sh
+++ debug/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sim=0.01
-epochs=50
+epochs=25
 gpu=1
 
 if [ ! -d "logs" ]
@@ -9,8 +9,24 @@ then
   mkdir logs
 fi
 
-nohup python train_vae.py --gpu $gpu --id 0 --epochs $epochs --sim $sim > logs/0.out 2>&1 &
-nohup python train_vae.py --gpu $gpu --id 1 --epochs $epochs --sim $sim > logs/1.out 2>&1 &
-nohup python train_vae.py --gpu $gpu --id 2 --epochs $epochs --sim $sim > logs/2.out 2>&1 &
-nohup python train_vae.py --gpu $gpu --id 3 --epochs $epochs --sim $sim > logs/3.out 2>&1 &
-nohup python train_vae.py --gpu $gpu --id 4 --epochs $epochs --sim $sim > logs/4.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 0 --epochs $epochs --sim $sim > logs/0_sim=$sim.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 1 --epochs $epochs --sim $sim > logs/1_sim=$sim.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 2 --epochs $epochs --sim $sim > logs/2_sim=$sim.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 3 --epochs $epochs --sim $sim > logs/3_sim=$sim.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 4 --epochs $epochs --sim $sim > logs/4_sim=$sim.out 2>&1 &
+
+sim=0.05
+nohup python train_vae.py --gpu $gpu --id 0 --epochs $epochs --sim $sim > logs/0_sim=$sim.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 1 --epochs $epochs --sim $sim > logs/1_sim=$sim.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 2 --epochs $epochs --sim $sim > logs/2_sim=$sim.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 3 --epochs $epochs --sim $sim > logs/3_sim=$sim.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 4 --epochs $epochs --sim $sim > logs/4_sim=$sim.out 2>&1 &
+
+sim=0.1
+nohup python train_vae.py --gpu $gpu --id 0 --epochs $epochs --sim $sim > logs/0_sim=$sim.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 1 --epochs $epochs --sim $sim > logs/1_sim=$sim.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 2 --epochs $epochs --sim $sim > logs/2_sim=$sim.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 3 --epochs $epochs --sim $sim > logs/3_sim=$sim.out 2>&1 &
+nohup python train_vae.py --gpu $gpu --id 4 --epochs $epochs --sim $sim > logs/4_sim=$sim.out 2>&1 &
+
+
